2e2d30b6a3d9d6bda2e6786b904affb1bc0908e2
chenjvnpu <chenjvnpu@163.com>
2017-06-27 23:06:25 +0800

    获取对象位置编辑器工具

---
+++ youyou/Assets/Editor/GetObjPath.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class GetObjPath : Editor {
+
+	[MenuItem("Assets/GetItemPath")]
+	static void getItemPath(){
+		//		Object[] seletionObjs	= Selection.GetFiltered (typeof(object),SelectionMode.DeepAssets);
+		Object activeObj = Selection.activeObject;
+		string path = AssetDatabase.GetAssetPath(activeObj);
+		Debug.Log (path);
+
+	}
+
+}
